146cbeee1c122a00c3f25c34eada641f203d813f
Josh Taylor <joshotaylor@gmail.com>
2012-09-27 16:37:00 +0100

    Refactored toggle flashlight code

--- src/com/jtws/tawch/Tawch.java
+++ src/com/jtws/tawch/Tawch.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.ads.*;
 
@@ -23,13 +25,10 @@ public class Tawch extends Activity {
 	
 	private Button toggleButton;
 	private Camera camera;
-	private Camera.Parameters cameraParameters;
 	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.main);
         
         // Setup click listeners for all the buttons
@@ -60,69 +59,128 @@ public class Tawch extends Activity {
     	window.setFormat(PixelFormat.RGBA_8888);
     }
     
-    /*
-     * Method used to toggle the flash on/off
+    /**
+     * Initialises the camera if not already started
      * 
-     * Depending on settings it should either blank screen
-     * or turn the flash on
+     * @return True on success, false on failure
+     */
+    public boolean initCamera() {
+    	if (camera == null) {
+    		camera = Camera.open();
+    	}
+    	return true;
+    }
+    
+    /**
+     * Releases the cameras resources and sets the camera property to null
+     * 
+     * @return True on success, false on failure
+     */
+    public boolean releaseCamera() {
+    	if (camera != null) {
+    		camera.release();
+    		camera = null;
+    	}
+    	return true;
+    }
+    
+    /**
+     * Uses two different methods to attempt to set the camera flash mode to on/off
+     * 
+     * @param isOn Whether to turn the camera on, or off.
+     * @return True on success, false on failure
+     */
+    public boolean setFlashMode(boolean isOn) {
+    	if (camera == null) {
+    		return false;
+    	}
+    	String value;
+    	
+    	// Attempt 1
+    	if (isOn) {
+    		value = Camera.Parameters.FLASH_MODE_TORCH;
+    	} else {
+    		value = Camera.Parameters.FLASH_MODE_OFF;
+    	}
+    	if (setCameraParameters(isOn, value, 1)) {
+    		return true;
+    	}
+    	
+    	// Attempt 2
+    	if (isOn) {
+    		value = "torch";
+    	} else {
+    		value = "off";
+    	}
+    	if (setCameraParameters(isOn, value, 2)) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    /**
+     * Sets the cameras parameters, uses two different methods
+     * 
+     * @param isOn Whether the flash light should be set on, or off
+     * @param value The value to use when setting the parameter
+     * @param attempt The attempt number, this changes which method is used *Ugly hack, I know*
+     * @return True on success, false on failure
+     */
+    public boolean setCameraParameters(boolean isOn, String value, int attempt) {
+    	Camera.Parameters params = camera.getParameters();
+    	try {
+    		if (attempt == 1) {
+    			params.setFlashMode(value);
+    		}
+    		if (attempt == 2) {
+    			params.set("flash-mode", value);
+    		}
+    		camera.setParameters(params);
+    		camera.startPreview();
+    		
+    		String currentMode = camera.getParameters().getFlashMode();
+    		if (isOn && currentMode.equals(Camera.Parameters.FLASH_MODE_TORCH)) {
+    			return true;
+    		}
+    		if (!isOn && currentMode.equals(Camera.Parameters.FLASH_MODE_OFF)) {
+    			return true;
+    		}
+    		return false;
+    	} catch (Exception e) {
+    		Log.e(TAG, "Error setting flash mode to " + value);
+    		return false;
+    	}
+    }
+    
+    /**
+     * Starts the ScreenTorch activity
+     */
+    public void startScreenTorch() {
+    	Intent i = new Intent(this, ScreenTorch.class);
+    	startActivity(i);
+    }
+    
+    /**
+     * Toggles the flash based on the current setting. If the flash could not be turned
+     * on it will resort to using the ScreenTorch activity. Also takes into account
+     * whether or not a users preference is to use the screen
      */
     public void toggleFlash() {
     	if(Prefs.getFlashType(this).equals("screen") || camera == null) {
     		Log.d(TAG, "Preferences state to use phone screen");
-    		
-    		Intent i = new Intent(this, ScreenTorch.class);
-    		startActivity(i);
+    		startScreenTorch();
     		return;
     	}
     	
-    	// Get the current camera parameters
-		cameraParameters = camera.getParameters();
-    	
-    	// Reverse torchState;
     	torchState = !torchState;
-    	
-    	String flashState;
-    	
-    	if(torchState) {
-    		flashState = Camera.Parameters.FLASH_MODE_TORCH;
-    	} else {
-    		flashState = Camera.Parameters.FLASH_MODE_OFF;
-    	}
-    	
-    	// First attempt at setting the camera parameters
-    	try {
-    		Log.d(TAG, "Settings camera parameter to " + flashState);
-    		
-    		cameraParameters.setFlashMode(flashState);
-    		camera.setParameters(cameraParameters);
-
-    		camera.startPreview();
-    	} catch(Exception e) {
-    		Log.e(TAG, "Error setting camera parameter to " + flashState, e);
-    		Log.d(TAG, "Trying a bit of a hack...");
-    		
-    		// That failed, so try a different technique
-    		try {
-    			if(torchState) {
-    				flashState = "torch";
-    			} else {
-    				flashState = "off";
-    			}
-    			
-    			cameraParameters.set("flash-mode", flashState);
-    			camera.setParameters(cameraParameters);
-    			
-    			camera.startPreview();
-    		} catch(Exception e2) {
-    			Log.e(TAG, "Failed with the hack, using the phones screen instead", e2);
-    			
-    			if(camera != null) {
-    				camera.release();
-    				camera = null;
-    			}
-    			
-    			Intent i = new Intent(this, ScreenTorch.class);
-    			startActivity(i);
+    	if (!torchState && Build.MANUFACTURER.equalsIgnoreCase("Samsung")) {
+    		this.releaseCamera();
+    		this.initCamera();
+    	} else if (!setFlashMode(torchState)) {
+    		if (torchState) {
+	    		Toast.makeText(this, "Cannot use flash, please report this error " +
+	    				"with your phones model. Thanks.", Toast.LENGTH_LONG).show();
+    			startScreenTorch();
     			return;
     		}
     	}
@@ -143,15 +201,8 @@ public class Tawch extends Activity {
 		// Reset the button text and torch_state
 		Button toggleButton = (Button)findViewById(R.id.turn_on_button);
 		toggleButton.setText(R.string.turn_on_label);
-		
 		torchState = false;
-    	
-		// Release the camera
-		if(camera != null) {
-			camera.release();
-			camera = null;
-		}
-		
+		releaseCamera();
 		super.onPause();
     }
     
@@ -160,17 +211,17 @@ public class Tawch extends Activity {
     	super.onResume();
     	// Attempt to reopen the camera
     	try {
-    		camera = Camera.open();
+    		initCamera();
     	} catch(Exception e) {
     		Log.e(TAG, "Error opening camera", e);
     		
-    		if(camera != null) {
-    			camera.release();
-    			camera = null;
-    		}
+    		releaseCamera();
     	}
     }
     
+    /**
+     * Shows a share intent menu so that the application can be shared
+     */
     public void showShareMenu() {
     	Intent shareIntent = new Intent(Intent.ACTION_SEND);
     	shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
